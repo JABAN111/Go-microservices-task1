0418f8b926f15fd8c84e5606536b41679cafaca2
jaba <368601@edu.itmo.ru>
2025-02-16 03:29:44 +0300

    add quote to compose

--- hello/internal/apiserver/config.go
+++ hello/internal/apiserver/config.go
@@ -1,1 +1,13 @@
 package apiserver
+
+type Config struct {
+	BindPort string `yaml:"port" env:"HELLO_PORT" default:"9001"`
+	BindHost string `yaml:"host" env:"HELLO_HOST" default:"localhost"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BindPort: "9001",
+		BindHost: "0.0.0.0",
+	}
+}
--- hello/internal/apiserver/server.go
+++ hello/internal/apiserver/server.go
@@ -1,11 +1,74 @@
-package main
+package apiserver
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
-func main() {
-	time.Sleep(5 * time.Second)
-	fmt.Println("hello!!!")
+type Server struct {
+	mux    *http.ServeMux
+	config *Config
+}
+
+func NewServer(config *Config) *Server {
+	s := Server{
+		http.NewServeMux(),
+		config,
+	}
+	return &s
+}
+
+func (s *Server) handlePing() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := fmt.Fprintf(w, "pong\n")
+		if err != nil {
+			log.Panicf("failed to write pong: %v", err)
+			return
+		}
+	}
+}
+
+func (s *Server) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+
+		if len(name) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err := fmt.Fprintf(w, "empty name\n")
+			if err != nil {
+				log.Panicf("failed to write hello: %v", err)
+			}
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, err := fmt.Fprintf(w, "Hello, %s!\n", name)
+		if err != nil {
+			log.Panicf("failed to write hello: %v", err)
+		}
+
+	}
+}
+
+func addRoutes(
+	s *Server,
+) {
+
+	s.mux.HandleFunc("GET /ping", s.handlePing())
+	s.mux.HandleFunc("GET /hello", s.handleHello())
+	//s.mux.Handle("/", http.NotFoundHandler())
+}
+
+func (s *Server) Run() {
+	addRoutes(s)
+
+	serverAddress := s.config.BindHost + ":" + s.config.BindPort
+	log.Printf("Server started on address %s", serverAddress)
+
+	err := http.ListenAndServe(serverAddress, s.mux)
+	if err != nil {
+		log.Panicf("Cannot started a server application, reason: %v", err)
+	}
 }
