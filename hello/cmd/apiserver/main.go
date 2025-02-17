fda0d32b0bb5cec82d02a0d46c1e716b08c797a9
jaba <368601@edu.itmo.ru>
2025-02-17 18:24:52 +0300

    update configuration of hello config

--- hello/cmd/apiserver/main.go
+++ hello/cmd/apiserver/main.go
@@ -24,18 +24,9 @@ func init() {
 func getConfig(configPath string) *apiserver.Config {
 	config := apiserver.NewConfig()
 
-	if err := cleanenv.ReadEnv(config); err == nil {
-		if config.BindPort != "" {
-			log.Println("Using environment variables for configuration")
-			return config
-		}
-	}
-
-	log.Printf("Cannot find environment variables, trying config file %s...", configPath)
-
 	if err := cleanenv.ReadConfig(configPath, config); err == nil {
 		if config.BindPort != "" {
-			log.Printf("Using config file: %s", configPath)
+			log.Printf("Using configuration: %s", config)
 			return config
 		}
 	}
